900f6583a1ae6c914ec39cd56b33fb848aea3225
dancefactory <youngkinam@yahoo.com>
2023-01-07 17:44:29 +0900

    add consumer

--- consumer.py
+++ consumer.py
@@ -5,9 +5,20 @@ c=Consumer({'bootstrap.servers':'imcmaster.iptime.org:29093','group.id':'python-
 print('Kafka Consumer has been initiated...')
 
 print('Available topics to consume: ', c.list_topics().topics)
-c.subscribe(['registered_user'])
 
-def main():
+def consume_once():
+    c.subscribe(['registered_user'])
+    msg=c.poll(1.0) #timeout
+    if msg is None:
+        print('No message received')
+        return
+    else:
+        data=msg.value().decode('utf-8')
+        print(data)
+    c.close()
+
+def consume_loop():
+    c.subscribe(['registered_user'])
     while True:
         msg=c.poll(1.0) #timeout
         if msg is None:
@@ -20,4 +31,4 @@ def main():
     c.close()
 
 if __name__ == "__main__":
-    main()
+    consume_loop()()
